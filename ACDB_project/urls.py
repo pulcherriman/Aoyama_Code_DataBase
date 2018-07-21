5abd020cc83d76b709963ab69911544a0d6530da
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2018-07-21 13:21:06 +0900

    ルートから/indexにリダイレクト

--- ACDB_project/urls.py
+++ ACDB_project/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^ACDB_app/', include('ACDB_app.urls')),
+    url(r'^', include('ACDB_app.urls')),
 ]
